a72b480464afabda54c422ee445082c2598cebcf
Luiz Fernando <luiz.silva@concrete.com.br>
2020-03-05 17:11:59 -0300

    Removing debug draw

--- Sample/SampleApp/ImagineUISample.swift
+++ Sample/SampleApp/ImagineUISample.swift
@@ -242,11 +242,6 @@ class ImagineUI: Blend2DSample {
         for window in windows {
             window.renderRecursive(in: ctx, region: redrawRegion)
         }
-        
-        // Debug draw
-        for window in windows {
-            DebugDraw.debugDrawRecursive(window, flags: [.constraints], to: ctx)
-        }
 
         redrawRegion.clear()
     }
