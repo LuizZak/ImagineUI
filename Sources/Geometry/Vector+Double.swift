a086683277b4823250e4a72276839cc2d947f6ba
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-08-17 06:53:04 -0300

    Adding SIMD specializations to Vector.ratio

--- Sources/Geometry/Vector+Double.swift
+++ Sources/Geometry/Vector+Double.swift
@@ -72,6 +72,24 @@ public extension VectorT where T == Double {
     func cross(_ other: VectorT) -> Double {
         return simd.cross(theVector, other.theVector).z
     }
+    
+    /// Returns the vector that lies within this and another vector's ratio line
+    /// projected at a specified ratio along the line created by the vectors.
+    ///
+    /// A vector on ratio of 0 is the same as this vector's position, and 1 is the
+    /// same as the other vector's position.
+    ///
+    /// Values beyond 0 - 1 range project the point across the limits of the line.
+    ///
+    /// - Parameters:
+    ///   - ratio: A ratio (usually 0 through 1) between this and the second vector.
+    ///   - other: The second vector to form the line that will have the point
+    /// projected onto.
+    /// - Returns: A vector that lies within the line created by the two vectors.
+    @inlinable
+    func ratio(_ ratio: T, to other: VectorT) -> VectorT {
+        return VectorT(mix(self.theVector, other.theVector, t: ratio))
+    }
 }
 
 // MARK: - Rotation
