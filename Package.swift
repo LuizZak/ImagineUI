689da650dc821a2bad70d0cecb7a92428ebe8fab
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-08-23 18:42:04 -0300

    Updating Geometria dependency

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["ImagineUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/LuizZak/Geometria.git", .branch("nosimd-vectortype")),
+        .package(url: "https://github.com/LuizZak/Geometria.git", .branch("main")),
         .package(url: "https://github.com/LuizZak/CassowarySwift.git", .branch("master")),
         .package(url: "https://github.com/LuizZak/swift-blend2d.git", .branch("master")),
         .package(url: "https://github.com/LuizZak/swift-libpng.git", .branch("master"))
--- Sources/Geometry/Geometry.swift
+++ Sources/Geometry/Geometry.swift
@@ -4,10 +4,10 @@ import simd
 public typealias Vector = SIMD2<Double>
 public typealias Size = Vector
 public typealias IntPoint = Vector2i
-public typealias Rectangle = Geometria.Rectangle<Vector>
+public typealias Rectangle = Geometria.NRectangle<Vector>
 public typealias EdgeInsets = EdgeInsets2<Vector>
 public typealias RoundRectangle = RoundRectangle2<Vector>
 public typealias Circle = Geometria.NSphere<Vector>
-public typealias Ellipse = Ellipse2<Vector>
+public typealias Ellipse = Ellipsoid<Vector>
 public typealias Line = Geometria.Line<Vector>
 public typealias Polygon = Geometria.LinePolygon<Vector>
