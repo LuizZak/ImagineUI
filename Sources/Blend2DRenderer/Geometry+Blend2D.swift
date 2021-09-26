9ce34ac04d89c8518be2abd68ebfaf64cf6a6c84
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-09-26 15:22:09 -0300

    Marking some declarations as public

--- Sources/Blend2DRenderer/Geometry+Blend2D.swift
+++ Sources/Blend2DRenderer/Geometry+Blend2D.swift
@@ -1,115 +1,115 @@
 import Geometry
 import SwiftBlend2D
 
-extension UILine {
+public extension UILine {
     var asBLLine: BLLine {
         return BLLine(start: start.asBLPoint, end: end.asBLPoint)
     }
 }
 
-extension BLLine {
+public extension BLLine {
     var asLine: UILine {
         return UILine(x1: x0, y1: y0, x2: x1, y2: y1)
     }
 }
 
-extension BLPoint {
+public extension BLPoint {
     var asVector2: UIVector {
         return UIVector(x: x, y: y)
     }
 }
 
-extension UIVector {
+public extension UIVector {
     var asBLPoint: BLPoint {
         return BLPoint(x: x, y: y)
     }
 }
 
-extension BLPointI {
+public extension BLPointI {
     var asIntPoint: UIIntPoint {
         return UIIntPoint(x: Int(x), y: Int(y))
     }
 }
 
-extension UIIntPoint {
+public extension UIIntPoint {
     var asBLPointI: BLPointI {
         return BLPointI(x: Int32(x), y: Int32(y))
     }
 }
 
-extension UIMatrix {
+public extension UIMatrix {
     var asBLMatrix2D: BLMatrix2D {
         return BLMatrix2D(m00: m11, m01: m12, m10: m21, m11: m22, m20: m31, m21: m32)
     }
 }
 
-extension UIRectangle {
+public extension UIRectangle {
     var asBLRect: BLRect {
         return BLRect(x: x, y: y, w: width, h: height)
     }
 }
 
-extension BLRect {
+public extension BLRect {
     var asRectangle: UIRectangle {
         return UIRectangle(x: x, y: y, width: w, height: h)
     }
 }
 
-extension BLBox {
+public extension BLBox {
     var asRectangle: UIRectangle {
         return UIRectangle(x: x0, y: y0, width: w, height: h)
     }
 }
 
-extension BLRectI {
+public extension BLRectI {
     var asRectangle: UIRectangle {
         return UIRectangle(x: Double(x), y: Double(y), width: Double(w), height: Double(h))
     }
 }
 
-extension BLBoxI {
+public extension BLBoxI {
     var asRectangle: UIRectangle {
         return UIRectangle(x: Double(x0), y: Double(y0), width: Double(w), height: Double(h))
     }
 }
 
-extension BLRoundRect {
+public extension BLRoundRect {
     var asRoundRectangle: UIRoundRectangle {
         return UIRoundRectangle(rectangle: UIRectangle(x: x, y: y, width: w, height: h), radiusX: rx, radiusY: ry)
     }
 }
 
-extension UIRoundRectangle {
+public extension UIRoundRectangle {
     var asBLRoundRect: BLRoundRect {
         return BLRoundRect(rect: rectangle.asBLRect, radius: BLPoint(x: radius.x, y: radius.y))
     }
 }
 
-extension BLCircle {
+public extension BLCircle {
     var asCircle: UICircle {
         return UICircle(center: center.asVector2, radius: r)
     }
 }
 
-extension UICircle {
+public extension UICircle {
     var asBLCircle: BLCircle {
         return BLCircle(center: center.asBLPoint, radius: radius)
     }
 }
 
-extension BLEllipse {
+public extension BLEllipse {
     var asEllipse: UIEllipse {
         return UIEllipse(center: UIVector(x: cx, y: cy), radiusX: rx, radiusY: ry)
     }
 }
 
-extension UIEllipse {
+public extension UIEllipse {
     var asBLEllipse: BLEllipse {
         return BLEllipse(center: center.asBLPoint, radius: BLPoint(x: radiusX, y: radiusY))
     }
 }
 
-extension UIEdgeInsets {
+public extension UIEdgeInsets {
     func inset(rectangle: BLRect) -> BLRect {
         return BLRect(x: rectangle.x + left,
                       y: rectangle.y + top,
@@ -118,7 +118,7 @@ extension UIEdgeInsets {
     }
 }
 
-extension UITriangle {
+public extension UITriangle {
     var asBLTriangle: BLTriangle {
         .init(p0: p0.asBLPoint, p1: p1.asBLPoint, p2: p2.asBLPoint)
     }
