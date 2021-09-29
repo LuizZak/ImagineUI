9716233d70c2012991bd812ba5ba7c730d4b939a
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-09-29 14:30:44 -0300

    Adding copy-mutators to KeyEventArgs

--- Sources/ImagineUI/Events/Keyboard/KeyEventArgs.swift
+++ Sources/ImagineUI/Events/Keyboard/KeyEventArgs.swift
@@ -10,4 +10,31 @@ public class KeyEventArgs {
         self.modifiers = modifiers
         self.handled = false
     }
+
+    /// Creates a copy of this event, modifying the return value's `keyCode` 
+    /// to a specified value.
+    public func withKeyCode(_ keyCode: Keys) -> KeyEventArgs {
+        let result = KeyEventArgs(keyCode: keyCode, keyChar: keyChar, modifiers: modifiers)
+        result.handled = handled
+
+        return result
+    }
+
+    /// Creates a copy of this event, modifying the return value's `keyChar` 
+    /// to a specified value.
+    public func withKeyChar(_ keyChar: String?) -> KeyEventArgs {
+        let result = KeyEventArgs(keyCode: keyCode, keyChar: keyChar, modifiers: modifiers)
+        result.handled = handled
+
+        return result
+    }
+
+    /// Creates a copy of this event, modifying the return value's `modifiers` 
+    /// to a specified value.
+    public func withModifiers(_ modifiers: KeyboardModifier) -> KeyEventArgs {
+        let result = KeyEventArgs(keyCode: keyCode, keyChar: keyChar, modifiers: modifiers)
+        result.handled = handled
+
+        return result
+    }
 }
