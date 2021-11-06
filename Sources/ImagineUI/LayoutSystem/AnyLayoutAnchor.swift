ab3c68f9cbbc595fe026e3ab71237161e94ac262
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-11-06 16:50:43 -0300

    Adding simplifications to constraint creation

--- Sources/ImagineUI/LayoutSystem/AnyLayoutAnchor.swift
+++ Sources/ImagineUI/LayoutSystem/AnyLayoutAnchor.swift
@@ -3,13 +3,13 @@ import CassowarySwift
 internal struct AnyLayoutAnchor: LayoutAnchorType, Equatable {
     weak var _owner: LayoutVariablesContainer?
     var kind: AnchorKind
-    
+
     var owner: AnyObject? { return _owner }
-    
+
     public var description: String {
         return getVariable()?.name ?? "<unowned anchor>"
     }
-    
+
     func getVariable() -> Variable? {
         switch kind {
         case .width:
@@ -35,28 +35,28 @@ internal struct AnyLayoutAnchor: LayoutAnchorType, Equatable {
 
     // Returns an expression that can be subtracted from this layout anchor's
     // `makeVariable` result to create an expression that is relative to another
-    // view's location
+    // view's location.
     func makeRelativeExpression(relative: LayoutVariablesContainer) -> Expression {
         switch kind {
         case .width, .height:
             return Expression(constant: 0)
-            
+
         case .left, .right, .centerX:
             return Expression(term: Term(variable: relative.layoutVariables.left))
-            
+
         case .top, .bottom, .centerY, .firstBaseline:
             return Expression(term: Term(variable: relative.layoutVariables.top))
         }
     }
-    
+
     func makeExpression(variable: Variable, relative: LayoutVariablesContainer) -> Expression {
         return variable - makeRelativeExpression(relative: relative)
     }
-    
+
     func isEqual(to other: AnyLayoutAnchor) -> Bool {
         return other._owner === _owner && other.kind == kind
     }
-    
+
     static func == (lhs: AnyLayoutAnchor, rhs: AnyLayoutAnchor) -> Bool {
         return lhs._owner === rhs._owner && lhs.kind == rhs.kind
     }
--- Sources/ImagineUI/LayoutSystem/Relationship.swift
+++ Sources/ImagineUI/LayoutSystem/Relationship.swift
@@ -4,7 +4,7 @@ public enum Relationship: CustomStringConvertible {
     case equal
     case greaterThanOrEqual
     case lessThanOrEqual
-    
+
     public var description: String {
         switch self {
         case .equal:
@@ -17,6 +17,10 @@ public enum Relationship: CustomStringConvertible {
     }
 
     func makeConstraint(left: Variable, right: Variable, offset: Double, multiplier: Double) -> Constraint {
+        if multiplier == 1 {
+            return makeConstraint(left: left, right: right, offset: offset)
+        }
+
         switch self {
         case .equal:
             return left == (right * multiplier) + offset
@@ -28,6 +32,10 @@ public enum Relationship: CustomStringConvertible {
     }
 
     func makeConstraint(left: Variable, right: Expression, offset: Expression, multiplier: Double) -> Constraint {
+        if multiplier == 1 {
+            return makeConstraint(left: left, right: right, offset: offset)
+        }
+
         switch self {
         case .equal:
             return left == (right * multiplier) + offset
@@ -39,6 +47,10 @@ public enum Relationship: CustomStringConvertible {
     }
 
     func makeConstraint(left: Variable, right: Variable, offset: Double) -> Constraint {
+        if offset == 0 {
+            return makeConstraint(left: left, right: right)
+        }
+
         switch self {
         case .equal:
             return left == right + offset
@@ -48,7 +60,40 @@ public enum Relationship: CustomStringConvertible {
             return left <= right + offset
         }
     }
-    
+
+    func makeConstraint(left: Variable, right: Expression, offset: Expression) -> Constraint {
+        switch self {
+        case .equal:
+            return left == right + offset
+        case .greaterThanOrEqual:
+            return left >= right + offset
+        case .lessThanOrEqual:
+            return left <= right + offset
+        }
+    }
+
+    func makeConstraint(left: Variable, right: Expression) -> Constraint {
+        switch self {
+        case .equal:
+            return left == right
+        case .greaterThanOrEqual:
+            return left >= right
+        case .lessThanOrEqual:
+            return left <= right
+        }
+    }
+
+    func makeConstraint(left: Variable, right: Variable) -> Constraint {
+        switch self {
+        case .equal:
+            return left == right
+        case .greaterThanOrEqual:
+            return left >= right
+        case .lessThanOrEqual:
+            return left <= right
+        }
+    }
+
     func makeConstraint(left: Variable, offset: Double) -> Constraint {
         switch self {
         case .equal:
