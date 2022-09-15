82f20a5cfc157acb9cf1ed34b3f5fea6a3342be4
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2022-09-15 18:06:31 -0300

    Adding public Color <-> BLRgba32 conversion properties

--- Sources/Blend2DRenderer/Color+Blend2D.swift
+++ Sources/Blend2DRenderer/Color+Blend2D.swift
@@ -2,7 +2,13 @@ import SwiftBlend2D
 import Rendering
 
 extension Color {
-    var asBLRgba32: BLRgba32 {
+    public var asBLRgba32: BLRgba32 {
         return BLRgba32(r: UInt32(red), g: UInt32(green), b: UInt32(blue), a: UInt32(alpha))
+    }
+}
+
+extension BLRgba32 {
+    public var asColor: Color {
+        return Color(alpha: Int(a), red: Int(r), green: Int(g), blue: Int(b))
     }
 }
